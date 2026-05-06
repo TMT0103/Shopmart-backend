ea2b2b19d7ba4e806246e7d09d1c7af53242735a
TMT0103 <tranminhtuan010305@gmail.com>
2026-05-06 10:38:46 +0700

    update

--- src/main/java/com/Ecommerce/Backend/security/SecurityConfig.java
+++ src/main/java/com/Ecommerce/Backend/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://your-frontend.vercel.app"
+                "https://shopmart-frontend-kappa.vercel.app/"
         )); // React
         config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
